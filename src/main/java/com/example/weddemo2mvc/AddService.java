6c3df777913db24f0f9a4784f4710912705307f5
Ulrika Eriksson <ulrika.m.a.eriksson@gmail.com>
2023-03-07 10:40:50 +0100

    Moved business logic to Addservice

---
+++ src/main/java/com/example/weddemo2mvc/AddService.java
@@ -0,0 +1,9 @@
+package com.example.weddemo2mvc;
+
+public class AddService {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+}
--- src/main/java/com/example/weddemo2mvc/AdderController.java
+++ src/main/java/com/example/weddemo2mvc/AdderController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
+
 @Controller
 public class AdderController {
+
+    private AddService addService;
 
     @GetMapping("/add")
     public String addForm() {
@@ -17,14 +22,14 @@ public class AdderController {
 
     @GetMapping("/add2/{tal1}/{tal2}")
     public String add2(@PathVariable int tal1, @PathVariable int tal2, Model m) {
-        int sum = tal1 + tal2;
+        int sum = addService.add(tal1, tal2);
         m.addAttribute("summa", sum);
         return "addresult";
     }
 
     @PostMapping("/add")
     public String add(@RequestParam int i, @RequestParam int j, Model m) {
-        int sum = i + j;
+        int sum = addService.add(i, j);
         m.addAttribute("summa", sum);
         return "addresult";
     }
